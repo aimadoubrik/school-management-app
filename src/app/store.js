5229ba0b65547f65f0b2d6ced53bea8b3a83f123
AbdelazizDoBronx <abdelzizaitmhnd8@gmail.com>
2024-11-11 20:56:31 +0100

    i did only add quizslice to the rootReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import notificationsReducer from '../features/notifications/notificationsSlice';
+import themeReducer from '../features/theme/themeSlice';
+import sidebarReducer from '../features/sidebar/sidebarSlice';
+import uiReducer from '../features/ui/uiSlice';
 import QuizzesReducer from '../features/Quiz/quizzesSlice';
+
+
+const rootReducer = combineReducers({
+  notifications: notificationsReducer,
+  theme: themeReducer,
+  sidebar: sidebarReducer,
+  ui: uiReducer,
+  quizzes : QuizzesReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    quizzes : QuizzesReducer
-  },
+  reducer: rootReducer,
 });
 
 export default store;
