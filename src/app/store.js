8519a6b766510eff6e4c8496e6a16d8d480e0754
Abdelilah07 <mabroukabdelilah07@gmail.com>
2024-11-17 13:19:42 +0100

    Improved DocumentsPage.

--- src/app/store.js
+++ src/app/store.js
@@ -12,8 +12,7 @@ import {
   scheduleReducer,
   profileSlice,
   documentsReducer,
-  moduleReducer,
-  profileSlice
+  moduleReducer
 } from '../features';
 
 const rootReducer = combineReducers({
@@ -28,8 +27,7 @@ const rootReducer = combineReducers({
   schedule: scheduleReducer,
   profile: profileSlice,
   documents: documentsReducer,
-  modules: moduleReducer,
-  profile: profileSlice
+  modules: moduleReducer
 });
 
 const store = configureStore({
