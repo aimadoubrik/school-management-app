37d3c18b958e7b67c2cbbdd589fbd0d45fa3d0b1
devowfs201 <devowfs201@proton.me>
2024-11-27 13:00:40 +0100

    adding a new chatbot

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,11 @@
 import QuickActions from './components/QuickActions';
 import ChatbotIframe from './components/ChatbotIframe';
+import ChatbotIframe2 from './components/ChatbotIframe2';
 function HomePage() {
   return (
     <div className="flex justify-center flex-wrap gap-4">
       <QuickActions />
-      <ChatbotIframe />
+      <ChatbotIframe2 chatbotId="9718397947" />
     </div>
     
   );
