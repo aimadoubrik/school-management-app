57b3ef6cb6b5efe09219b998044863e90e48c9f8
devowfs201 <devowfs201@proton.me>
2024-11-27 11:13:32 +0100

    adding chatbot

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,12 @@
 import QuickActions from './components/QuickActions';
+import ChatbotIframe from './components/ChatbotIframe';
 function HomePage() {
   return (
     <div className="flex justify-center flex-wrap gap-4">
       <QuickActions />
+      <ChatbotIframe />
     </div>
+    
   );
 }
 
---
+++ src/pages/Home/components/ChatbotIframe.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+const ChatbotIframe = () => {
+  useEffect(() => {
+    window.embeddedChatbotConfig = {
+      chatbotId: "2RWLFtBve7klLfmH0LaS3",
+      domain: "www.chatbase.co",
+    };
+
+    const script = document.createElement('script');
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.setAttribute('chatbotId', "2RWLFtBve7klLfmH0LaS3");
+    script.setAttribute('domain', "www.chatbase.co");
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ChatbotIframe;
