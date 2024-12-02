de0881f16d0a0010ca9ac895b09e73e290d4e1cd
aimad <aimad.ofppt@gmail.com>
2024-12-02 18:17:33 +0100

    First Version of Document Generator

---
+++ src/pages/Documents/Docs/TemplatesDoc/Diplome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diplome = () => {
+    return (
+        <div className="bg-white p-8 w-[210mm] h-[297mm] mx-auto">
+            <h1 className="text-2xl font-bold text-center text-gray-900 border-2 border-gray-300 py-4 rounded-lg ">Diplome</h1>
+        </div>
+    );
+};
+
+export default Diplome;
+
