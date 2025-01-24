f42306e711052a0fd05dbe4a4e82fc10cf995b11
d-anasx <djamaouianas@gmail.com>
2025-01-24 23:05:52 +0100

    .

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,12 @@
 import QuickActions from './components/QuickActions';
 import GreetingHeader from './components/GreetingHeader';
-import ChatlingBotAdmin from './components/ChatlingBotAdmin';
-import ChatlingBotFormateur from './components/ChatlingBotFormateur';
+
 function HomePage() {
   return (
     <div className="flex justify-center flex-wrap gap-4">
       <GreetingHeader />
       <QuickActions />
-      <ChatlingBotAdmin />
+      
     </div>
   );
 }
